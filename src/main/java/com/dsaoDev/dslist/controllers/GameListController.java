4d19580c1d476fe53d2e9e62d211b888acbf3653
dsaoDev <davissgt1@hotmail.com>
2023-05-09 23:31:09 -0300

    Find by list

--- src/main/java/com/dsaoDev/dslist/controllers/GameListController.java
+++ src/main/java/com/dsaoDev/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dsaoDev.dslist.dto.GameListDTO;
+import com.dsaoDev.dslist.dto.GameMinDTO;
 import com.dsaoDev.dslist.services.GameListService;
+import com.dsaoDev.dslist.services.GameService;
 
 @RestController 
 @RequestMapping(value = "/lists")
@@ -16,7 +19,15 @@ public class GameListController {
 	
 	@Autowired
 	private GameListService gameListService;
+	@Autowired
+	private GameService gameService;	
 	
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List<GameMinDTO> result =  gameService.findByList(listId);
+		return result;
+	}
 	
 	
 	@GetMapping
--- src/main/java/com/dsaoDev/dslist/services/GameService.java
+++ src/main/java/com/dsaoDev/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dsaoDev.dslist.dto.GameDTO;
 import com.dsaoDev.dslist.dto.GameMinDTO;
 import com.dsaoDev.dslist.entities.Game;
+import com.dsaoDev.dslist.projections.GameMinProjection;
 import com.dsaoDev.dslist.repositories.GameRepository;
 
 @Service
@@ -29,5 +30,11 @@ public class GameService {
 		List<Game> result =  gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result =  gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
